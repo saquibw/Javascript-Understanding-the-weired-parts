a70c5a9b990a9cd34330b458f9be23c51f744795
saquibw <saquib.waheed@yahoo.com>
2019-03-14 00:20:42 +0600

    Asynchronous callback understood

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // Syntax parsers:  
-// Lexical environments
+// Lexical environments: where a piece of code physically leaves
 // Global execution context
 
 // object: Collection of name/value pairs
@@ -44,6 +44,7 @@
 
 // Variable environment: Where the variable lives (memory space). Every execution context has it's own distinct variable environment. 
 // For global execution context, it's variable environment is the window object.
+// When a function is declared, it also gets added to the variable environment of that execution context
 
 function b() {
     var myVar;
@@ -61,5 +62,22 @@ console.log(myVar);
 a();
 
 // Scope chain
+// Every execution context has a reference to it's outer environment (variable environment). Means, where the function is lexically located.
+// In case of defining outer reference, Javascript cares about the lexical environment, means where the function is located physically.
+// If a variable can't be find in the current execution context, javascript will look at the outer reference, where the function/ variable is located physically
+// This going down the line is called scope chain
 
-//Scope: Where a variable is available in your code
+// Scope: Where a variable is available in your code. Where I can access a variable.
+// All these things, Lexical environment, variable environment, execution context are in total defining what is called scope
+// 'let' allowes block scoping, means, it's lifecycle ends with the block inside which it's defined.
+
+// Asynchronous: More than one at a time
+// Asynchronous callback: 
+// A browser has multiple engines: Javascript engine, Rendering engine (vor view), HTTP Request (for requests)
+// Javascript engine is synchronous. But is has to talk to other engines. That's  being done through asynchronous callback.
+// Asynchronous means, what's happening outside the JS engine
+// Besides execution stack, there is another structure/ list in JS engine => Event Queue
+// If browser, outside of JS engine has an event, it puts that event inside event queue asynchronously
+// When the execution stack is empty, JS engine looks at event queue periodically for events (this is called event loop)
+// A eventHandler function is bind with that event. JS engine creates execution context for that and executes the function
+// An event is being handled by a event handler
